3c8e24db6546f528f5aaf02e3f51d0c1af407f39
jmlee <jmlee@tonesol.com>
2024-07-16 09:45:34 +0900

    0716

---
+++ node2/app.js
@@ -0,0 +1,28 @@
+// Package 
+require('dotenv').config();
+const express=require('express');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser')   
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+// HTML 파일 위치 views
+nunjucks.configure('views',{
+    express:app,
+});
+
+// html 환경 설정, post, public
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({extended:false})); //객체 들어감. 추가 2 
+app.use(express.static('public'));
+
+// Controller
+// 127.0.0.1/
+app.get('/', (req,res)=>{
+    res.render('index');
+});
+
+app.listen(port,()=>{
+    console.log(`server start port:${port}`)
+})
+
