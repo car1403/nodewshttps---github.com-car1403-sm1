7618d271fe044b2761da3cc09ada8f18460eed41
jmlee <jmlee@tonesol.com>
2024-07-22 15:35:13 +0900

    0722

---
+++ node2/test/test.js
@@ -0,0 +1,7 @@
+let v1 = 0x6211002bb464e5842ff32c8b9b98d2a2892e33e4;
+let v2 = '0x6211002bb464e5842ff32c8b9b98d2a2892e33e4';
+
+console.log(v1);
+console.log(v1.toString(16));
+console.log(v2);
+console.log(v1+' ');
